89faaea1586cbcd1cfd63b05db7b65e08c2d73f7
Nov3m63r <no.v3m63r@gmail.com>
2020-01-07 16:38:41 +0300

    Day 1 tasks added (4, 7, 8)

---
+++ day_01/day1_4.py
@@ -0,0 +1,12 @@
+# https://www.jetbrains.com/help/pycharm/creating-and-running-your-first-python-project.html
+
+# 1_4
+name = "Yoalli"
+number_of_apples = 15
+some_text = "Hello " + name + ". I hope you are well! Apparently you have " + str(number_of_apples) + " apples."
+print(some_text)
+
+#1_6
+#print("Hello, World!")
+
+
