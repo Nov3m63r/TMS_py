195614676c937eb9965f066e1e40868469369ec3
Nov3m63r <no.v3m63r@gmail.com>
2020-01-28 20:01:39 +0300

    day 7 task 1 - ready

--- day_07a/day7_1.py
+++ day_07a/day7_1.py
@@ -1,6 +1,29 @@
 import json
+import csv
 
-with open('day7_1.json', 'r') as json_file:
-    json_str = json_file.read()
-    obj = json.loads(json_str)
+json_path = "json_file.json"
+csv_path = "day7_1.csv"
 
+with open(json_path) as json_file:
+    f = json_file.read()
+    json_str = json.loads(f)
+
+keys = []
+for line in json_str:
+    key_line = list(dict.keys(line))
+    for key in key_line:
+        if key not in keys:
+            keys.append(key)
+
+
+with open(csv_path, "w") as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(keys)
+
+    for line in json_str:
+        row = []
+        for i in keys:
+            row.append(dict.get(line, i))
+        writer.writerow(row)
+
+print("csv file is ready")
