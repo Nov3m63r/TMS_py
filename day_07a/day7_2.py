6baac1abca5ff7700d8131831d4dda21b936feff
Nov3m63r <no.v3m63r@gmail.com>
2020-01-30 18:46:02 +0300

    Half of day7-2 task

--- day_07a/day7_2.py
+++ day_07a/day7_2.py
@@ -13,15 +13,26 @@ json_file = 'json_file7_2.json'
 
 wb = openpyxl.load_workbook(xls_file)
 sheet = wb.active
-print(sheet)
+#print(sheet)
+i = 1
+fio = []
+for row in sheet.iter_rows(min_row=1, max_col=2):
+    # print(sheet[f'A{i}'])
+    # fio = sheet[f'A{i}'].value
+    fio.append(sheet[f'A{i}'].value + ' ' + sheet[f'B{i}'].value)
+    i += 1
+    print(fio)
+
+# for row in sheet.iter_rows(min_row=1, max_col=2):
+#     sheet.merge_cells(start_row=i, start_column=1, end_row=i, end_column=2)
+
+# sheet.insert_cols(1)
 
 # https://openpyxl.readthedocs.io/en/stable/tutorial.html
-# построчно считать лист
-for row in ...
-    # ws['A4'] = 4
-    # d = ws.cell(row=4, column=2, value=10)
-# для каждой строки объединить 1 и 2 ячейку, сложить содержимое через пробел (Имя + Фамилия)
-#     сложить содержимое
+# +построчно считать лист
+# +for row in ...
+# +для каждой строки объединить 1 и 2 ячейку, сложить содержимое через пробел (Имя + Фамилия)
+# +    сложить содержимое
 #     объединить ячейки
 #     присвоить результат объединенной ячейке
 # перед первой колонкой вставить колонку с формулой - средний балл для каждого студента
@@ -39,9 +50,9 @@ for row in ...
 # }
 wb.save(xls_file)
 
-with open(json_file, 'w') as json_file:
-    json_str = json.dumps(data, indent=4)
-    print(json_str)
-    json_file.write(json_str)
+# with open(json_file, 'w') as json_file:
+#     json_str = json.dumps(data, indent=4)
+#     print(json_str)
+#     json_file.write(json_str)
 
 # print("json file is ready")
